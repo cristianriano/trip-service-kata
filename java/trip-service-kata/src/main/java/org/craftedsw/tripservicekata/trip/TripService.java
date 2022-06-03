1bbb3c5725ac382438e4e29c9b13a92c43a27644
Eldar Singin <eldar.singin@glovoapp.com>
2022-06-03 17:12:00 +0300

    Refactoring

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,41 +1,35 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
+import java.util.Optional;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = getUserFromSession();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = findTripsByUser(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
+    public List<Trip> getTripsByUser(final User user) throws UserNotLoggedInException {
+        final User loggedUser = Optional.ofNullable(getUserFromSession())
+                                        .orElseThrow(UserNotLoggedInException::new);
 
-	protected List<Trip> findTripsByUser(User user) {
-		return TripDAO.findTripsByUser(user);
-	}
+        return areFriends(user, loggedUser) ?
+            findTripsByUser(user)
+            : Collections.emptyList();
+    }
 
-	protected User getUserFromSession() {
-		return UserSession.getInstance()
-						  .getLoggedUser();
-	}
+    private boolean areFriends(User user, User another) {
+        return user.getFriends().stream()
+                   .anyMatch(friend -> friend.equals(another));
+    }
+
+    protected List<Trip> findTripsByUser(User user) {
+        return TripDAO.findTripsByUser(user);
+    }
+
+    protected User getUserFromSession() {
+        return UserSession.getInstance()
+                          .getLoggedUser();
+    }
 
 }
