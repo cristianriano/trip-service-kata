3567ed2a029610e090a88e7863cff2dd82d6e99b
Eldar Singin <eldar.singin@glovoapp.com>
2022-06-03 16:38:12 +0300

    Added 2 tests

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,17 +1,50 @@
 package org.craftedsw.tripservicekata.trip;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
 
+import java.util.List;
+import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class TripServiceTest {
 
+    @Spy
+    private TripService testedInstance = new TripService();
 
 
-  @Test
-  void getTripsByUser_whenUserHasNoFriends_returnsEmptyList() {
+    @Test
+    void getTripsByUser_whenUserHasNoFriends_returnsEmptyList() {
+        // Given
+        User user = new User();
+        doReturn(user).when(testedInstance)
+                      .getUserFromSession();
 
-  }
+        // When
+        List<Trip> tripsByUser = testedInstance.getTripsByUser(user);
+
+        // Then
+        assertTrue(tripsByUser.isEmpty());
+    }
+
+    @Test
+    void getTripsByUser_whenUserHasFriends_returnsListOfTrips() {
+        // Given
+        User user = new User();
+        user.addFriend(user);
+        doReturn(user).when(testedInstance)
+                      .getUserFromSession();
+
+        // When
+        List<Trip> tripsByUser = testedInstance.getTripsByUser(user);
+
+        // Then
+        assertTrue(tripsByUser.isEmpty());
+    }
 
 //  If is not logged in raise error
 //  If returns trips of friends only
