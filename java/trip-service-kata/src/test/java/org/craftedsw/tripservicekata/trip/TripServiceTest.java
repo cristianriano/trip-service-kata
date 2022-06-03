07339f7ebb53815c72eeb73b5556f84fc254350a
Cristian Riano <cristiancarm11@gmail.com>
2022-06-03 15:25:41 +0200

    Wip

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,18 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class TripServiceTest {
-	
+
+
+
+  @Test
+  void getTripsByUser_whenUserHasNoFriends_returnsEmptyList() {
+
+  }
+
+//  If is not logged in raise error
+//  If returns trips of friends only
 }
