de4656f4329c33b7231ce38d650c45fe6c8943d1
Cristian Riano <cristiancarm11@gmail.com>
2022-06-04 13:14:57 +0200

    Add Test for when users are not friends

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -4,21 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
+import java.util.List;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
-import org.craftedsw.tripservicekata.user.UserSession;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class TripServiceTest {
 
+    public static final User CURRENT_USER = new User();
     private static final User user = new User();
 
     @Spy
@@ -26,11 +24,22 @@ public class TripServiceTest {
 
     @Test
     void getTripsByUser_whenUserIsNotLogged_raiseException() {
-        doReturn(null).when(tripService).getLoggedUser();
+        mockLoggedUser(null);
 
         assertThrows(
             UserNotLoggedInException.class,
             () -> tripService.getTripsByUser(user)
         );
+    }
+
+    @Test
+    void getTripsByUser_whenLoggedUserIsNotAFriend_returnEmptyList() {
+        mockLoggedUser(CURRENT_USER);
+
+        assertEquals(tripService.getTripsByUser(user), List.of());
+    }
+
+    private void mockLoggedUser(User user) {
+        doReturn(user).when(tripService).getLoggedUser();
     }
 }
